0727ff5e2432f6ddb9832bc64df1eeff756d6dfa
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-25 13:41:39 -0400

    Initial FizzBuzz solution.
    Working fizzbuzz. started to make some changes to do it anothter way,

--- ConditionalLab/Program.cs
+++ ConditionalLab/Program.cs
@@ -45,6 +45,8 @@ namespace ConditionalLab
 
             // Time and classifications
 
+
+            /*
             DateTime moment = new System.DateTime();
             moment = DateTime.Now;
             int seconds = moment.Second;
@@ -72,11 +74,25 @@ namespace ConditionalLab
             }
 
             Console.ReadLine();
+            */
 
 
+            // FizzBuzz
 
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("Fizzbuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
 
+            }
 
+            Console.ReadLine();
 
         }
     }
