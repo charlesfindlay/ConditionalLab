2b54bbfdeeee7def45f7d8cefabcc6265e768f6b
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-25 11:10:30 -0400

    Start

---
+++ ConditionalLab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
